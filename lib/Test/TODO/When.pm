a7d01acfa19847fe141c279ef22f96c92953aa5f
tmurray <tmurray@wumpus-cave.net>
2012-06-15 14:00:50 -0500

    Initial layout

---
+++ lib/Test/TODO/When.pm
@@ -0,0 +1,95 @@
+package Test::TODO::When;
+
+use v5.12;
+
+=head1 NAME
+
+Test::TODO::When - The great new Test::TODO::When!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::TODO::When;
+
+    my $foo = Test::TODO::When->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head1 AUTHOR
+
+Timm Murray, C<< <tmurray at wumpus-cave.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-todo-when at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-TODO-When>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::TODO::When
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-TODO-When>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-TODO-When>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-TODO-When>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test-TODO-When/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Timm Murray.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Test::TODO::When
